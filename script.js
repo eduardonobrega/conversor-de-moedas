6abd359cd63f4ffc6a25795e21db4dec7fdd02b7
Eduardo Nóbrega <87456011+eduardonobrega@users.noreply.github.com>
2022-03-13 23:37:15 -0300

    JS e API

---
+++ script.js
@@ -0,0 +1,55 @@
+var money = document.getElementById('money')
+
+var price = 5.08
+// config From
+var convertFrom = document.getElementById('from')
+var imgFrom = document.getElementsByTagName('img')[0]
+var coinFrom = document.getElementById('coinFrom')
+var valueFrom = document.getElementById('valueFrom')
+
+// config to
+var imgTo = document.getElementsByTagName('img')[2]
+var coinTo = document.getElementById('coinTo')
+var valueTo = document.getElementById('valueTo')
+var convertTo = document.getElementById('to')
+
+function changed() { // trocando imagens, textos da div resultado e cotação da moeda
+
+    if (convertFrom.value == 'real') {
+        imgFrom.src = './img/brasil.png'
+        coinFrom.innerText = `Real`
+    }
+
+    if (convertTo.value == 'dolar') {
+        imgTo.src = './img/estados-unidos.png'
+        coinTo.innerText = 'Dólar Americano'
+        price = 5.08
+    } else if (convertTo.value == 'euro') {
+        imgTo.src = './img/euro.png'
+        coinTo.innerText = 'Euro'
+        price = 2
+    }
+    convert()
+
+}
+
+money.setAttribute('placeholder', 'R$ 10.000,00')
+
+async function convert() {
+    let moedas = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL").then( function(resposta) {
+        return resposta.json()
+    })
+    let dolar = moedas.USDBRL.high
+    let euro  = moedas.EURBRL.high
+ 
+    var conversion = 0
+    if (convertFrom.value == 'real' && convertTo.value == 'dolar') {
+        conversion =  money.value / dolar
+        valueFrom.innerText = `${Number(money.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
+        valueTo.innerText = `${conversion.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`
+    }else if (convertFrom.value == 'real' && convertTo.value == 'euro') {
+        conversion =  money.value / euro
+        valueFrom.innerText = `${Number(money.value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
+        valueTo.innerText = `${conversion.toLocaleString('en-GB', { style: 'currency', currency: 'EUR' })}`
+    }
+}
